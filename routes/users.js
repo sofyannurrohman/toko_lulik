5792dc72beb8fb031b3bed9cf3059850a104f1ad
sofyannurrohman <sofyannurrohman1@gmail.com>
2022-07-02 13:25:33 +0700

    first commit

---
+++ routes/users.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const usersHandler = require("./handler/users");
+
+/* GET users listing. */
+router.post("/register", usersHandler.register);
+
+module.exports = router;
